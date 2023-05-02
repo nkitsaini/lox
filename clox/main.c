156a2b2153388778b2bdee6f7771c21e5568069a
Ankit Saini <nnkitsaini@gmail.com>
2023-05-02 16:17:36 +0530

    Adding support for constant storage

--- clox/main.c
+++ clox/main.c
@@ -5,12 +5,12 @@
 int main(int argc, const* argv[]) {
 	Chunk chunk;
 	initChunk(&chunk);
-	writeChunk(&chunk, OP_RETURN);
+	writeChunk(&chunk, OP_RETURN, 1);
 
 	int constant = addConstant(&chunk, 1.2);
-	writeChunk(&chunk, OP_CONSTANT);
+	writeChunk(&chunk, OP_CONSTANT, 2);
 	printf("Constant Add: %d", constant);
-	writeChunk(&chunk, constant);
+	writeChunk(&chunk, constant, 2);
 
 	disassembleChunk(&chunk, "test chunk");
 	freeChunk(&chunk);
