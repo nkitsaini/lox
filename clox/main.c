a3a6b914ee8fe1c750f9ff1d381ed163b35d3322
Ankit Saini <nnkitsaini@gmail.com>
2023-05-04 16:47:45 +0530

    Using zig compiler (yup, yup, now zig)

--- clox/main.c
+++ clox/main.c
@@ -2,7 +2,7 @@
 #include "chunk.h"
 #include "debug.h"
 
-int main(int argc, const* argv[]) {
+int main(int argc, char* argv[]) {
 	Chunk chunk;
 	initChunk(&chunk);
 	writeChunk(&chunk, OP_RETURN, 1);
