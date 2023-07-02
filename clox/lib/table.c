296823a631e4052377b3d021b129f34f5cb11ccc
Ankit Saini <nnkitsaini@gmail.com>
2023-07-02 14:19:07 +0530

    perf improvement

--- clox/lib/table.c
+++ clox/lib/table.c
@@ -1,5 +1,3 @@
-#include "table.h"
-
 #include <stdlib.h>
 #include <string.h>
 
@@ -22,7 +20,7 @@ void freeTable(Table *table) {
 }
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
-  uint32_t index = key->hash % capacity;
+  uint32_t index = key->hash & (capacity - 1);
   Entry *tombstone = NULL;
   for (;;) {
     Entry *entry = &entries[index];
@@ -35,7 +33,7 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
         if (tombstone == NULL) tombstone = entry;
       }
     }
-    index = (index + 1) % capacity;
+    index = (index + 1) & (capacity - 1);
   }
 }
 
@@ -116,7 +114,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length,
   if (table->count == 0) {
     return NULL;
   }
-  uint32_t index = hash % (table->capacity);
+  uint32_t index = hash & (table->capacity - 1);
   for (;;) {
     Entry *entry = &table->entries[index];
 
@@ -128,7 +126,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length,
                memcmp(entry->key->chars, chars, length) == 0) {
       return entry->key;
     }
-    index = (index + 1) % table->capacity;
+    index = (index + 1) & (table->capacity - 1);
   }
 }
 
